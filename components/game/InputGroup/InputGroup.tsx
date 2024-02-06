cb1511a4037bb40f8faaf6793d1129417e7a56fe
Jan Balin <jgbalin@gmail.com>
2024-02-06 17:10:26 +0100

    more

--- components/game/InputGroup/InputGroup.tsx
+++ components/game/InputGroup/InputGroup.tsx
@@ -62,7 +62,7 @@ export default function InputGroup(props: InputProps) {
   return (
     <form
       action={async () => {}}
-      className="flex border-2 rounded-lg p-4 flex-row gap-x-8"
+      className="flex border-2 bg-amber-500 rounded-lg p-2 px-4 justify-evenly flex-row gap-x-8"
     >
       {loading ? (
         <div className="min-w-screen min-h-screen absolute bg-red-300">
@@ -71,76 +71,79 @@ export default function InputGroup(props: InputProps) {
       ) : (
         <></>
       )}
-      <div id="sliders" className="flex  flex-col">
-        <h2 className="text-4xl">Custom settings:</h2>
-        <div className="flex flex-row">
-          <label className="" htmlFor="snippetLengthSlider">
-            Words in snippets:
-          </label>
-          <input
-            className="text-center w-8 my-2 bg-zinc-700 text-white"
-            type="text"
-            name=""
-            id="snippetLengthSsider"
-            value={snippetLengthValue}
-            onFocus={(e) => {
-              e.target.select();
-            }}
-            onChange={(e) => {
-              if (parseInt(e.target.value) > 100) {
-                setSnippetLengthValue("100");
-              } else {
-                setSnippetLengthValue(e.target.value);
-              }
-            }}
-          />
+      <div id="sliders" className="flex gap-y-2 flex-col">
+        <h2 className="text-2xl w-full text-center">Custom settings:</h2>
+        <div className="flex flex-col gap-y-2">
+          <div className="flex flex-row gap-x-2 justify-end">
+            <label className="w-full" htmlFor="snippetsAmountSlider">
+              Number of snippets:{" "}
+            </label>
+            <input
+              min="2"
+              max="5"
+              step="1"
+              type="range"
+              id="snippetLengthChoice"
+              className="w-32"
+              onChange={(val) => setSnippetAmount(val.target.value)}
+              value={snippetAmount}
+            />
+            <input
+              className="w-8  text-center bg-zinc-700 text-white"
+              type="text"
+              name=""
+              id="snippetsAmountSlider"
+              value={snippetAmount}
+              onFocus={(e) => {
+                e.target.select();
+              }}
+              onChange={(e) => {
+                if (parseInt(e.target.value) > 10) {
+                  setSnippetAmount("10");
+                } else {
+                  setSnippetAmount(e.target.value);
+                }
+              }}
+            />
+          </div>
+          <div className="flex flex-row gap-x-2 justify-end">
+            <label className="w-full" htmlFor="snippetLengthSlider">
+              Words in snippets:
+            </label>
+            <input
+              min="0"
+              max="50"
+              step="5"
+              type="range"
+              id="snippetLengthChoice"
+              className="w-32"
+              onChange={(val) => setSnippetLengthValue(val.target.value)}
+              value={snippetLengthValue}
+            />
+            <input
+              className="text-center w-8  bg-zinc-700 text-white"
+              type="text"
+              name=""
+              id="snippetLengthSsider"
+              value={snippetLengthValue}
+              onFocus={(e) => {
+                e.target.select();
+              }}
+              onChange={(e) => {
+                if (parseInt(e.target.value) > 100) {
+                  setSnippetLengthValue("100");
+                } else {
+                  setSnippetLengthValue(e.target.value);
+                }
+              }}
+            />
+          </div>
         </div>
-        <input
-          min="0"
-          max="50"
-          step="5"
-          type="range"
-          id="snippetLengthChoice"
-          className="w-full"
-          onChange={(val) => setSnippetLengthValue(val.target.value)}
-          value={snippetLengthValue}
-        ></input>
-
-        <div className="flex flex-row">
-          <label htmlFor="snippetsAmountSlider">Number of snippets: </label>
-          <input
-            className="w-8 my-2 text-center bg-zinc-700 text-white"
-            type="text"
-            name=""
-            id="snippetsAmountSlider"
-            value={snippetAmount}
-            onFocus={(e) => {
-              e.target.select();
-            }}
-            onChange={(e) => {
-              if (parseInt(e.target.value) > 10) {
-                setSnippetAmount("10");
-              } else {
-                setSnippetAmount(e.target.value);
-              }
-            }}
-          />
-        </div>
-        <input
-          min="2"
-          max="5"
-          step="1"
-          type="range"
-          id="snippetLengthChoice"
-          className="w-full"
-          onChange={(val) => setSnippetAmount(val.target.value)}
-          value={snippetAmount}
-        />
       </div>
 
-      <div className="flex flex-col ">
-        <h2 className="text-4xl">Custom settings:</h2>
-        <div id="buttons" className="flex flex-shrink flex-wrap ">
+      <div className="flex flex-col gap-y-2">
+        <h2 className="text-2xl w-full text-center">Premade settings:</h2>
+        <div id="buttons" className="flex flex-shrink gap-x-2 flex-wrap ">
           <button
             className="gamecontrol_button difficulty_button  bg-green-600  "
             onClick={() => {
@@ -179,6 +182,7 @@ export default function InputGroup(props: InputProps) {
           </button>
           <div className="flex items-center justify-around ">
             <button
+              className="text-xs md:text-sm lg:text-lg font-semibold border-4 border-rose-400 border-opacity-20  w-[16rem] px-4 py-3 bg-purple-600 transition hover:duration-[250ms] hover:ease-in-out hover:bg-fuchsia-700"
               type="submit"
               onClick={(e) => {
                 e.preventDefault();
--- components/game/PlayingField/SnippetTitle.tsx
+++ components/game/PlayingField/SnippetTitle.tsx
@@ -1,8 +1,8 @@
+import { WikiDocument } from "@/resources/WikiHelperTypes";
 import { useEffect } from "react";
-import { WikiPageObject } from "../../resources/WikiHelperTypes";
 
 interface SnippetTitleProps {
-  wikiPage: WikiPageObject;
+  wikiPage: WikiDocument;
   dragStartHandler: (event: React.DragEvent<HTMLDivElement>) => void;
   dragEndHandler: (event: React.DragEvent<HTMLDivElement>) => void;
   htmlId: string;
@@ -12,25 +12,25 @@ function SnippetTitle(props: SnippetTitleProps) {
   useEffect(() => {
     const el: HTMLElement | null = document.getElementById(props.htmlId);
     const length = props.wikiPage.title.length;
-    if (length > 40) {
-      el!.style.fontSize = "0.75rem";
-      el!.style.lineHeight = "1rem";
-    } else {
-      el!.style.fontSize = "0.875rem";
-      el!.style.lineHeight = "1.25rem";
-    }
+    // if (length > 40) {
+    //   el!.style.fontSize = "0.75rem";
+    //   el!.style.lineHeight = "1rem";
+    // } else {
+    //   el!.style.fontSize = "0.875rem";
+    //   el!.style.lineHeight = "1.25rem";
+    // }
   });
 
   return (
-    <div
+    <li
       onDragStart={props.dragStartHandler}
       onDragEnd={props.dragEndHandler}
       draggable="true"
-      className={`bg-slate-600 grid wikiTitle rounded-[1rem] lg:rounded-[2rem] p-2 lg:p-4 items-center justify-center hover:cursor-move`}
+      className="wikiTitle font-semibold w-auto text-base bg-blue-500  rounded-[1rem] lg:rounded-[2rem] p-2 lg:p-4 items-center justify-center hover:cursor-move"
       id={props.htmlId}
     >
       {props.wikiPage.title}
-    </div>
+    </li>
   );
 }
 
