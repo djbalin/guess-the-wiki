81ede40152510a0cb77b537a128f1703733d9fc2
Jan-Georges Jersild Balin <jgbalin@gmail.com>
2026-05-16 14:47:58 +0200

    setup hono

---
+++ app/api/[[...routes]]/route.ts
@@ -0,0 +1,18 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+const app = new Hono().basePath("/api");
+
+app.get("/", (c) => {
+  return c.json({
+    message: "Welcome to the API!!",
+  });
+});
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
